43a8e782060ffd7d2bd402f2e8afa64a5b9a7f5d
jerisan <jerilynn.sancho@gmail.com>
2023-08-07 20:57:30 -0500

    React-escultura

--- src/App.js
+++ src/App.js
@@ -1,25 +1,146 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { sculptureList } from './data.js';
 
-function App() {
+const Gallery = () => {
+  const [index, setIndex] = useState(0);
+  const [showMore,setShowMore] = useState(false);
+
+  const handleClick = () => {
+    if(index<(sculptureList.length)-1){
+      setIndex(index + 1);
+    }else{
+      setIndex(0);
+      //alert('...');
+    }
+    //setIndex((oldValue => oldValue + 1));
+    //setIndex(index + 1);
+    console.log(index);
+  }
+
+  const handleMoreClick = () => {
+    setShowMore(!showMore);
+  }
+  //console.log('uno',index);
+  
+  let sculpture = sculptureList[index];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={handleClick}>
+        Siguiente
+      </button>
+      <h2>
+        <i>{sculpture.name} </i> 
+        por {sculpture.artist}
+      </h2>
+      <h3>  
+        ({index + 1} de {sculptureList.length})
+      </h3>
+      <button onClick={handleMoreClick}>
+        {showMore ? 'Ocultar':'Mostrar'} detalles
+      </button>
+      <img 
+        src={sculpture.url} 
+        alt={sculpture.alt}
+      />
+      {showMore &&<p>{sculpture.description} </p>}
+    </>
   );
 }
 
-export default App;
+export default Gallery;
+//Otra solucion para que no tenga error al momento de dar siguiente
+// export default Gallery;
+
+// export default function Gallery() {
+//   const [index, setIndex] = useState(0);
+//   const [showMore, setShowMore] = useState(false);
+
+//   let hasPrev = index > 0;
+//   let hasNext = index < sculptureList.length - 1;
+
+//   function handlePrevClick() {
+//     if (hasPrev) {
+//       setIndex(index - 1);
+//     }
+//   }
+
+//   function handleNextClick() {
+//     if (hasNext) {
+//       setIndex(index + 1);
+//     }
+//   }
+
+//   function handleMoreClick() {
+//     setShowMore(!showMore);
+//   }
+
+//   let sculpture = sculptureList[index];
+//   return (
+//     <>
+//       <button
+//         onClick={handlePrevClick}
+//         disabled={!hasPrev}
+//       >
+//         Anterior
+//       </button>
+//       <button
+//         onClick={handleNextClick}
+//         disabled={!hasNext}
+//       >
+//         Siguiente
+//       </button>
+//       <h2>
+//         <i>{sculpture.name} </i> 
+//         por {sculpture.artist}
+//       </h2>
+//       <h3>  
+//         ({index + 1} de {sculptureList.length})
+//       </h3>
+//       <button onClick={handleMoreClick}>
+//         {showMore ? 'Ocultar' : 'Mostrar'} detalles
+//       </button>
+//       {showMore && <p>{sculpture.description}</p>}
+//       <img 
+//         src={sculpture.url} 
+//         alt={sculpture.alt}
+//       />
+//     </>
+//   );
+// }
+
+
+
+/*******VARIABLE REGULAR - al dar click no cambiara la informacion********** */
+// import { sculptureList } from './data.js';
+
+// export default function Gallery() {
+//   let index = 0;
+
+//   function handleClick() {
+//     index = index + 1;
+//   }
+
+//   let sculpture = sculptureList[index];
+//   return (
+//     <>
+//       <button onClick={handleClick}>
+//         Siguiente
+//       </button>
+//       <h2>
+//         <i>{sculpture.name} </i> 
+//         por {sculpture.artist}
+//       </h2>
+//       <h3>  
+//         ({index + 1} de {sculptureList.length})
+//       </h3>
+//       <img 
+//         src={sculpture.url} 
+//         alt={sculpture.alt}
+//       />
+//       <p>
+//         {sculpture.description}
+//       </p>
+//     </>
+//   );
+// }
